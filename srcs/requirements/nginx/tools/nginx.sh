af83cd48c76a18bd823b8447058e807a97a86af0
mertilhans <160387403+mertilhans@users.noreply.github.com>
2026-06-28 22:41:07 +0300

    Delete srcs directory

--- srcs/requirements/nginx/tools/nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/nginx/ssl/nginx.key \
-    -out /etc/nginx/ssl/nginx.crt \
-    -subj "/C=TR/ST=Kocaeli/L=Kocaeli/O=42/OU=42/CN=merilhan.42.fr"
-
-exec nginx -g 'daemon off;'
